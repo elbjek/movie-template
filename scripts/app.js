9354751219ef3d2ff51eab8e6e8f74c379e9e861
elbjek <lenabjekovic@hotmaill.com>
2018-05-20 23:25:00 +0200

    second push

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,56 @@
 
 Vue.component('card-template',{
   props:['card'],
-  template: '<p>{{card.Title}}</p>'
+  template: '<div class="single-card"><img v-bind:src="card.Poster" alt="Missing a image"><p>Title:{{card.Title}}</p> <p>Year:{{card.Year}}</p></div>'
 })
 
 var app = new Vue({
   el:'#app',
   data:{
     item:'',
-    results:[],
-    searching:false,
-    noresult:false,
-
+    results: [],
+    noresult:false
   },
   methods:{
-    search:function(){
 
-      this.searching=true;
-      fetch('http://www.omdbapi.com/?s='+encodeURIComponent(this.item) +'&apikey=e8ed16e8')
-      .then(response => response.json())
-      .then((data)=>{
-        console.log(data);
-        this.results = data;
-        this.searching = false;
-        document.querySelector("input").style.width="500px";
-        document.querySelector("input").style.height="40px";
-      })
+    search:function(){ 
+      var URL = 'http://www.omdbapi.com/?s='+encodeURIComponent(this.item)+'&apikey=e8ed16e8';  
+      var request = new XMLHttpRequest();
+      request.open('GET', URL);
+      request.onreadystatechange = function(){
+
+        if(request.readyState === XMLHttpRequest.DONE){
+          if(this.status === 200){
+            app.results = JSON.parse(this.response);
+            app.results = app.results.Search;
+            document.getElementById("cards").style.display="flex";
+          } 
+          if(this.item == null){
+            app.noresult=true;
+          }
+    
+        }
+      }
+      request.send();
+      document.querySelector("input").style.width="500px";
+      document.querySelector("input").style.height="40px";
+     
+      
     },
     spread:function(){
       document.querySelector("input").style.width="700px";
       document.querySelector("input").style.height="50px";
       document.querySelector("input").style.transition="0.6s ease-in-out";
+    },
+    scroll:function(){
+
     }
-  }
+
+    }
+    
+
 })
+
 
 
 // http://www.omdbapi.com/?s='+encodeURIComponent(this.item)+'&apikey=e8ed16e8'
